1c82a4deab17913d2976f119d4d9b5a479dfe76f
ivanmarkov97 <ivanmarkov1997@gmail.com>
2021-10-15 15:25:35 +0300

    Added sem 5 ref before sem. Deleted button Del

--- sem5/blueprints/basket/routes.py
+++ sem5/blueprints/basket/routes.py
@@ -1,42 +1,38 @@
 import os
 
-from flask import Blueprint
-from flask import session
-from flask import render_template, current_app, request, redirect
+from flask import (
+	Blueprint, session, render_template, current_app, request, redirect)
 
-from database.connection import DBConnection, make_request
+from database.connection import work_with_db
 from database.sql_provider import SQLProvider
-from utils import AccessManager
-from .utils import add_user_basket, clear_user_basket, remove_item_by_id
+from access import AccessManager
+from .utils import add_user_basket, clear_user_basket
 
 
-basket_pb = Blueprint('basket', __name__, template_folder='templates')
+basket_app = Blueprint('basket', __name__, template_folder='templates')
 provider = SQLProvider(os.path.join(os.path.dirname(__file__), 'sql'))
 
 
-@basket_pb.route('/', methods=['GET', 'POST'])
+@basket_app.route('/', methods=['GET', 'POST'])
 @AccessManager.group_required
 def list_orders():
 	if request.method == 'GET':
 		current_basket = session.get('basket', [])
-		items = make_request(current_app.config['DB_CONFIG'], provider.get('order_list.sql'))
+		items = work_with_db(current_app.config['DB_CONFIG'], provider.get('order_list.sql'))
 		return render_template('basket_order_list.html', items=items, basket=current_basket)
 	else:
 		action = request.form['action']
 		item_id = request.form['item_id']
 		if action == 'Add':
 			sql = provider.get('order_item.sql', item_id=item_id)
-			item = make_request(current_app.config['DB_CONFIG'], sql)
+			item = work_with_db(current_app.config['DB_CONFIG'], sql)
 			if item:
 				item = item[0]
-			else:
-				remove_item_by_id(item_id)
-				return render_template('basket_item_absent.html')
 			add_user_basket(item)
 		return redirect('/order')
 
 
-@basket_pb.route('/clear')
+@basket_app.route('/clear')
 @AccessManager.group_required
 def clear_basket():
 	clear_user_basket()
